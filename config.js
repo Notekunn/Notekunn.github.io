32a60564e16afbb95085c1b56a6397be0fc5dca0
Cuong Tran <clonebmn2itt@gmail.com>
2021-02-15 20:23:49 +0700

    first c

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: "Notekunn's Blog",
+  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/',
+    },
+    {
+      label: 'About me',
+      path: '/pages/about',
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts',
+    },
+  ],
+  author: {
+    name: 'Trần Đức Cường',
+    photo: '/photo.jpg',
+    bio: 'Sinh viên học viện KTMM',
+    contacts: {
+      email: 'clonebmn2itt@gmail.com',
+      facebook: 'ShiinDz',
+      telegram: '',
+      twitter: '',
+      github: 'Notekunn',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: 'UCPaBveCj-lurlLc-9chMvGQ',
+      soundcloud: '',
+      medium: '',
+    },
+  },
+};
